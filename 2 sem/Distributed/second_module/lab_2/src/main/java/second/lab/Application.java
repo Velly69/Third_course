bb8691109c44778154d7c1a5ee93aa0cbc7e2045
Alexander Totsky <43824249+Velly69@users.noreply.github.com>
2021-05-16 19:36:13 +0300

    added second lab in second module

---
+++ 2 sem/Distributed/second_module/lab_2/src/main/java/second/lab/Application.java
@@ -0,0 +1,340 @@
+package second.lab;
+
+import org.xml.sax.SAXException;
+import second.lab.connection.CustomConnection;
+import second.lab.dao.GenreDAO;
+import second.lab.dao.MovieDAO;
+import second.lab.dto.GenreDTO;
+import second.lab.dto.MovieDTO;
+
+import javax.swing.*;
+import javax.xml.parsers.ParserConfigurationException;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+import java.util.List;
+
+public class Application extends JFrame {
+    private static JFrame frame;
+
+    private static GenreDTO currentGenre = null;
+    private static MovieDTO currentMovie = null;
+
+    private static boolean editMode = false;
+    private static boolean genreMode = true;
+
+    private static JButton btnAddGenre = new JButton("Add Genre");
+    private static JButton btnAddMovie = new JButton("Add Movie");
+    private static JButton btnEdit = new JButton("Edit Data");
+    private static JButton btnBack = new JButton("Back");
+    private static JButton btnSave = new JButton("Save");
+    private static JButton btnDelete = new JButton("Delete");
+
+    private static Box menuPanel = Box.createVerticalBox();
+    private static Box actionPanel = Box.createVerticalBox();
+    private static Box comboPanel = Box.createVerticalBox();
+    private static Box cityPanel = Box.createVerticalBox();
+    private static Box countryPanel = Box.createVerticalBox();
+
+    private static JComboBox comboGenre = new JComboBox();
+    private static JComboBox comboMovie = new JComboBox();
+
+    private static JTextField textGenreName = new JTextField(30);
+    private static JTextField textMovieName = new JTextField(30);
+    private static JTextField textMovieGenreName = new JTextField(30);
+    private static JTextField textMovieYear = new JTextField(30);
+
+    private Application() {
+        super("Video Shop");
+        frame = this;
+        frame.setPreferredSize(new Dimension(400, 500));
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent event) {
+                frame.dispose();
+                CustomConnection.closeConnection();
+                System.exit(0);
+            }
+        });
+        Box box = Box.createVerticalBox();
+        sizeAllElements();
+        frame.setLayout(new FlowLayout());
+
+        // Menu
+        menuPanel.add(btnAddGenre);
+        menuPanel.add(Box.createVerticalStrut(20));
+        btnAddGenre.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent event) {
+                editMode = false;
+                genreMode = true;
+                menuPanel.setVisible(false);
+                comboPanel.setVisible(false);
+                countryPanel.setVisible(true);
+                cityPanel.setVisible(false);
+                actionPanel.setVisible(true);
+                pack();
+            }
+        });
+        menuPanel.add(btnAddMovie);
+        menuPanel.add(Box.createVerticalStrut(20));
+        btnAddMovie.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent event) {
+                editMode = false;
+                genreMode = false;
+                menuPanel.setVisible(false);
+                comboPanel.setVisible(false);
+                countryPanel.setVisible(false);
+                cityPanel.setVisible(true);
+                actionPanel.setVisible(true);
+                pack();
+            }
+        });
+        menuPanel.add(btnEdit);
+        menuPanel.add(Box.createVerticalStrut(20));
+        btnEdit.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent event) {
+                editMode = true;
+                menuPanel.setVisible(false);
+                comboPanel.setVisible(true);
+                countryPanel.setVisible(false);
+                cityPanel.setVisible(false);
+                actionPanel.setVisible(true);
+                pack();
+            }
+        });
+
+        // ComboBoxes
+        comboPanel.add(new JLabel("Genre:"));
+        comboPanel.add(comboGenre);
+        comboPanel.add(Box.createVerticalStrut(20));
+        comboGenre.addActionListener(e -> {
+            String name = (String) comboGenre.getSelectedItem();
+            currentGenre = GenreDAO.findByName(name);
+            genreMode = true;
+            countryPanel.setVisible(true);
+            cityPanel.setVisible(false);
+            fillGenreFields();
+            pack();
+        });
+        comboPanel.add(new JLabel("Movie:"));
+        comboPanel.add(comboMovie);
+        comboPanel.add(Box.createVerticalStrut(20));
+        comboMovie.addActionListener(e -> {
+            String name = (String) comboMovie.getSelectedItem();
+            currentMovie = MovieDAO.findByName(name);
+            genreMode = false;
+            countryPanel.setVisible(false);
+            cityPanel.setVisible(true);
+            fillMovieFields();
+            pack();
+        });
+        fillComboBoxes();
+        comboPanel.setVisible(false);
+
+        // City Fields
+        cityPanel.add(new JLabel("Name:"));
+        cityPanel.add(textMovieName);
+        cityPanel.add(Box.createVerticalStrut(20));
+        cityPanel.add(new JLabel("Genre Name:"));
+        cityPanel.add(textMovieGenreName);
+        cityPanel.add(Box.createVerticalStrut(20));
+        cityPanel.add(new JLabel("Release year:"));
+        cityPanel.add(textMovieYear);
+        cityPanel.add(Box.createVerticalStrut(20));
+        cityPanel.setVisible(false);
+
+        // Country Fields
+        countryPanel.add(new JLabel("Name:"));
+        countryPanel.add(textGenreName);
+        countryPanel.add(Box.createVerticalStrut(20));
+        countryPanel.setVisible(false);
+
+        // Action Bar
+        actionPanel.add(btnSave);
+        actionPanel.add(Box.createVerticalStrut(20));
+        btnSave.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent event) {
+                save();
+            }
+        });
+        actionPanel.add(btnDelete);
+        actionPanel.add(Box.createVerticalStrut(20));
+        btnDelete.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent event) {
+                delete();
+            }
+        });
+        actionPanel.add(btnBack);
+        actionPanel.add(Box.createVerticalStrut(20));
+        btnBack.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent event) {
+                clearFields();
+                menuPanel.setVisible(true);
+                comboPanel.setVisible(false);
+                countryPanel.setVisible(false);
+                cityPanel.setVisible(false);
+                actionPanel.setVisible(false);
+                pack();
+            }
+        });
+        actionPanel.setVisible(false);
+
+        clearFields();
+        box.setPreferredSize(new Dimension(300, 500));
+        box.add(menuPanel);
+        box.add(comboPanel);
+        box.add(countryPanel);
+        box.add(cityPanel);
+        box.add(actionPanel);
+        setContentPane(box);
+        //pack();
+    }
+
+    private static void sizeAllElements() {
+        Dimension dimension = new Dimension(300, 50);
+        btnAddGenre.setMaximumSize(dimension);
+        btnAddMovie.setMaximumSize(dimension);
+        btnEdit.setMaximumSize(dimension);
+        btnBack.setMaximumSize(dimension);
+        btnSave.setMaximumSize(dimension);
+        btnDelete.setMaximumSize(dimension);
+
+        btnAddGenre.setAlignmentX(Component.CENTER_ALIGNMENT);
+        btnAddMovie.setAlignmentX(Component.CENTER_ALIGNMENT);
+        btnSave.setAlignmentX(Component.CENTER_ALIGNMENT);
+        btnBack.setAlignmentX(Component.CENTER_ALIGNMENT);
+        btnEdit.setAlignmentX(Component.CENTER_ALIGNMENT);
+        btnDelete.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        Dimension panelDimension = new Dimension(300, 300);
+        menuPanel.setMaximumSize(panelDimension);
+        comboPanel.setPreferredSize(panelDimension);
+        actionPanel.setPreferredSize(panelDimension);
+        cityPanel.setPreferredSize(panelDimension);
+        countryPanel.setPreferredSize(panelDimension);
+
+        comboGenre.setPreferredSize(dimension);
+        comboMovie.setPreferredSize(dimension);
+
+        textMovieGenreName.setPreferredSize(dimension);
+        textMovieName.setPreferredSize(dimension);
+        textMovieYear.setPreferredSize(dimension);
+        textGenreName.setPreferredSize(dimension);
+    }
+
+    private static void save() {
+        if (editMode) {
+            if (genreMode) {
+                currentGenre.setName(textGenreName.getText());
+                if (!GenreDAO.update(currentGenre)) {
+                    JOptionPane.showMessageDialog(null, "Error: update failed!");
+                }
+            } else {
+                currentMovie.setName(textMovieName.getText());
+                currentMovie.setReleaseYear(Integer.parseInt(textMovieYear.getText()));
+
+                GenreDTO genre = GenreDAO.findByName(textMovieGenreName.getText());
+                if (genre == null) {
+                    JOptionPane.showMessageDialog(null, "Error: no such country!");
+                    return;
+                }
+                currentMovie.setGenreId(genre.getId());
+
+                if (!MovieDAO.update(currentMovie)) {
+                    JOptionPane.showMessageDialog(null, "Error: update failed!");
+                }
+            }
+        } else {
+            if (genreMode) {
+                GenreDTO genre = new GenreDTO();
+                genre.setName(textGenreName.getText());
+
+                if (!GenreDAO.insert(genre)) {
+                    JOptionPane.showMessageDialog(null, "Error: insertion failed!");
+                    return;
+                }
+
+                comboGenre.addItem(genre.getName());
+            } else {
+                MovieDTO movie = new MovieDTO();
+                movie.setName(textMovieName.getText());
+                movie.setReleaseYear(Integer.parseInt(textMovieYear.getText()));
+
+                GenreDTO genre = GenreDAO.findByName(textMovieGenreName.getText());
+                if (genre == null) {
+                    JOptionPane.showMessageDialog(null, "Error: no such country!");
+                    return;
+                }
+                movie.setGenreId(genre.getId());
+
+                if (!MovieDAO.insert(movie)) {
+                    JOptionPane.showMessageDialog(null, "Error: insertion failed!");
+                    return;
+                }
+
+                comboMovie.addItem(movie.getName());
+            }
+        }
+    }
+
+    private static void delete() {
+        if (editMode) {
+            if (genreMode) {
+                List<MovieDTO> list = MovieDAO.findByGenreId(currentGenre.getId());
+                for (MovieDTO m : list) {
+                    comboMovie.removeItem(m.getName());
+                    MovieDAO.delete(m);
+                }
+                GenreDAO.delete(currentGenre);
+                comboGenre.removeItem(currentGenre.getName());
+            } else {
+                MovieDAO.delete(currentMovie);
+                comboMovie.removeItem(currentMovie.getName());
+            }
+        }
+    }
+
+    private void fillComboBoxes() {
+        comboGenre.removeAllItems();
+        comboMovie.removeAllItems();
+        List<GenreDTO> genres = GenreDAO.findAll();
+        List<MovieDTO> movies = MovieDAO.findAll();
+        for (GenreDTO genre : genres) {
+            comboGenre.addItem(genre.getName());
+        }
+        for (MovieDTO movie : movies) {
+            comboMovie.addItem(movie.getName());
+        }
+    }
+
+    private static void clearFields() {
+        textGenreName.setText("");
+        textMovieName.setText("");
+        textMovieGenreName.setText("");
+        textMovieYear.setText("");
+        currentGenre = null;
+        currentMovie = null;
+    }
+
+    private static void fillGenreFields() {
+        if (currentGenre == null)
+            return;
+        textGenreName.setText(currentGenre.getName());
+    }
+
+    private static void fillMovieFields() {
+        if (currentMovie == null)
+            return;
+        GenreDTO genre = GenreDAO.findById(currentMovie.getGenreId());
+        textMovieName.setText(currentMovie.getName());
+        assert genre != null;
+        textMovieGenreName.setText(genre.getName());
+        textMovieYear.setText(String.valueOf(currentMovie.getReleaseYear()));
+    }
+
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+        JFrame myWindow = new Application();
+        myWindow.setVisible(true);
+    }
+}
---
+++ 2 sem/Distributed/second_module/lab_2/src/main/java/second/lab/dao/MovieDAO.java
@@ -0,0 +1,160 @@
+package second.lab.dao;
+
+import second.lab.connection.CustomConnection;
+import second.lab.dto.MovieDTO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieDAO {
+    public static MovieDTO findById(long id) {
+        try (Connection connection = CustomConnection.getConnection();) {
+            String sql = "SELECT * FROM movie WHERE id = ?";
+            assert connection != null;
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            MovieDTO movie = null;
+            if (resultSet.next()) {
+                movie = new MovieDTO();
+                movie.setMovieId(resultSet.getLong(1));
+                movie.setName(resultSet.getString(2));
+                movie.setReleaseYear(resultSet.getInt(3));
+                movie.setGenreId(resultSet.getLong(4));
+            }
+            preparedStatement.close();
+            return movie;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static MovieDTO findByName(String name) {
+        try (Connection connection = CustomConnection.getConnection();) {
+            String sql = "SELECT * FROM movie WHERE name = ?";
+            assert connection != null;
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, name);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            MovieDTO movie = null;
+            if (resultSet.next()) {
+                movie = new MovieDTO();
+                movie.setMovieId(resultSet.getLong(1));
+                movie.setName(resultSet.getString(2));
+                movie.setReleaseYear(resultSet.getInt(3));
+                movie.setGenreId(resultSet.getLong(4));
+            }
+            preparedStatement.close();
+            return movie;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static boolean update(MovieDTO movie) {
+        try (Connection connection = CustomConnection.getConnection();) {
+            String sql = "UPDATE movie SET name = ?, release_year = ?, genre_id = ? WHERE id = ?";
+            assert connection != null;
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, movie.getName());
+            preparedStatement.setInt(2, movie.getReleaseYear());
+            preparedStatement.setLong(3, movie.getGenreId());
+            preparedStatement.setLong(4, movie.getMovieId());
+            int result = preparedStatement.executeUpdate();
+            preparedStatement.close();
+            return result > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static boolean insert(MovieDTO movie) {
+        try (Connection connection = CustomConnection.getConnection();) {
+            String sql = "INSERT INTO movie (name, release_year, genre_id) VALUES (?,?,?) RETURNING id";
+            assert connection != null;
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, movie.getName());
+            preparedStatement.setInt(2, movie.getReleaseYear());
+            preparedStatement.setLong(3, movie.getGenreId());
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                movie.setMovieId(resultSet.getLong(1));
+            } else
+                return false;
+            preparedStatement.close();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static boolean delete(MovieDTO movie) {
+        try (Connection connection = CustomConnection.getConnection();) {
+            String sql = "DELETE FROM movie WHERE id = ?";
+            assert connection != null;
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setLong(1, movie.getMovieId());
+            int result = preparedStatement.executeUpdate();
+            preparedStatement.close();
+            return result > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static List<MovieDTO> findAll() {
+        try (Connection connection = CustomConnection.getConnection();) {
+            String sql = "SELECT * FROM movie";
+            assert connection != null;
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<MovieDTO> list = new ArrayList<>();
+            while (resultSet.next()) {
+                MovieDTO movie = new MovieDTO();
+                movie.setMovieId(resultSet.getLong(1));
+                movie.setName(resultSet.getString(2));
+                movie.setReleaseYear(resultSet.getInt(3));
+                movie.setGenreId(resultSet.getLong(4));
+                list.add(movie);
+            }
+            preparedStatement.close();
+            return list;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static List<MovieDTO> findByGenreId(Long id) {
+        try (Connection connection = CustomConnection.getConnection();) {
+            String sql = "SELECT * FROM movie WHERE genre_id = ?";
+            assert connection != null;
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<MovieDTO> list = new ArrayList<>();
+            while (resultSet.next()) {
+                MovieDTO movie = new MovieDTO();
+                movie.setMovieId(resultSet.getLong(1));
+                movie.setName(resultSet.getString(2));
+                movie.setReleaseYear(resultSet.getInt(3));
+                movie.setGenreId(resultSet.getLong(4));
+                list.add(movie);
+            }
+            preparedStatement.close();
+            return list;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
